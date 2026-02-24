dfee3b39292050e06aa2fe0ecfb15236fe3b796a
netman <netman@netman-VirtualBox>
2026-02-23 23:44:28 -0700

    pushing from VM! Look at this!

--- NMGithub.py
+++ NMGithub.py
@@ -4,15 +4,12 @@ import os
 
 local_url = "/home/netman/netman"
 url = "https://github.com/sash2087/netman.git"
-branch = "new"
+branch = "main"
 
 repo = git.Repo(local_url)
-
 repo.git.add('*')
-
 repo.index.commit("pushing from VM! Look at this!")
-
-origin = repo.remote(name = "origin")
+origin = repo.remote(name = "main")
 origin.push(branch)
 
 #WIP!!!!
